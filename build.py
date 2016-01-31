43e1d9d6771d75af394daac932211caa3ca19c88
Liang Hai <lianghai@gmail.com>
2016-01-31 18:25:36 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,47 +1,39 @@
 #! /usr/bin/env AFDKOPython
-
+# encoding: UTF-8
+from __future__ import division, absolute_import, print_function, unicode_literals
 import hindkit as kit
 
-family = kit.Family(
-    trademark = 'Kumar',
+family_filled = kit.Family(
+    client = 'Google Fonts',
+    trademark = 'Kumar One',
     script = 'Gujarati',
     hide_script_name = True,
 )
+family_filled.set_masters([kit.Master(family_filled, 'Regular', 0)])
+family_filled.set_styles(kit.constants.styles.SINGLE)
 
-family.set_masters(
-    masters = [kit.Master(family, 'Regular', 0)],
-    modules = [
-        # 'kerning',
-        # 'mark_positioning',
-        # 'mark_to_mark_positioning',
-        # 'devanagari_matra_i_variants',
-    ],
+family_outlined = kit.Family(
+    client = 'Google Fonts',
+    trademark = 'Kumar One Outline',
+    script = 'Gujarati',
+    hide_script_name = True,
 )
+family_outlined.set_masters([kit.Master(family_outlined, 'Regular', 0)])
+family_outlined.set_styles(kit.constants.styles.SINGLE)
 
-family.set_styles(kit.constants.styles.SINGLE)
-
-builder = kit.Builder(family)
-
-builder.fontrevision = '0.201'
-
-builder.set_options([
-
-    'prepare_styles',   # stage i
-    'prepare_features', # stage ii
-    'compile',          # stage iii
-
-    # 'makeinstances', #!
-    'checkoutlines', #!
-    # 'autohint',      #!
-
-    'do_style_linking',
-    'use_os_2_version_4',
-    'prefer_typo_metrics',
-    'is_width_weight_slope_only',
-
-])
-
-builder.generate_designspace()
-builder.generate_fmndb()
-
-builder.build()
+for family in [family_filled, family_outlined]:
+    builder = kit.Builder(
+        family,
+        fontrevision = '0.201',
+        vertical_metrics = {
+            'Ascender': 750,
+            'Descender': -250,
+            'LineGap': 200,
+        },
+        options = {
+            'prep_mark_positioning': True,
+            'override_GDEF': True,
+            'do_style_linking': True,
+        },
+    )
+    builder.build()
