b2981702fff5c9685deb388264b7c5ac170dbfbc
Liang Hai <lianghai@gmail.com>
2016-04-03 02:33:40 +0800

    Update build system

--- build.py
+++ build.py
@@ -3,41 +3,40 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 import hindkit as kit
 
-family = kit.Family(
-    client = 'Google Fonts',
-    script = 'Gujarati',
-    trademark = 'Kumar One',
-    designers = 'Parimal Parmar',
-)
-family.set_masters(
-    [kit.Master(family, _file_name='Kumar One-Filled.ufo')]
-)
-family.set_styles(kit.constants.styles.SINGLE)
+# ---
 
-family_outlined = kit.Family(
-    client = 'Google Fonts',
-    script = 'Gujarati',
-    trademark = 'Kumar One Outline',
-    designers = 'Parimal Parmar',
-)
-family_outlined.set_masters(
-    [kit.Master(family_outlined, _file_name='Kumar One-Outlined.ufo')]
-)
-family_outlined.set_styles(kit.constants.styles.SINGLE)
+for tag, family in [
+    (
+        'Filled', kit.Family(
+            client = 'Google Fonts',
+            trademark = 'Kumar One',
+            script = 'Gujarati',
+        ),
+    ),
+    (
+        'Outlined', kit.Family(
+            client = 'Google Fonts',
+            trademark = 'Kumar One Outline',
+            script = 'Gujarati',
+        ),
+    ),
+]:
 
-for f in [family, family_outlined]:
-    builder = kit.Builder(
-        f,
-        fontrevision = '0.900',
-        vertical_metrics = {
-            'Ascender': 750,
-            'Descender': -250,
-            'LineGap': 200,
-        },
+    family.set_styles(kit.constants.styles.SINGLE)
+
+    family.info.openTypeNameDesigner = "Parimal Parmar"
+    family.info.openTypeHheaAscender, family.info.openTypeHheaDescender = (1050, -450)
+
+    family.masters[0].filename = 'Kumar One-' + tag
+    family.styles[0].abstract_directory = 'styles/' + tag
+
+    project = kit.Project(
+        family,
+        fontrevision = '1.000',
         options = {
             # 'prepare_mark_positioning': True,
-            'override_GDEF': True,
             'do_style_linking': True,
+            'build_ttf': True,
         },
     )
-    builder.build()
+    project.build()
